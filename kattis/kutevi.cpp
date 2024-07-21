2b788943603dbc00bbfb07dada293b6ec3cf9ccd
Ziwen Wang <1308632188@qq.com>
2024-07-21 00:31:49 -0400

    update

--- kattis/kutevi.cpp
+++ kattis/kutevi.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+// Using number theory.
+// alpha . v = s iff s is multiple of gcd(v)
+// Where alpha and v are vectors and s is a number.
+/*
 void solve() {
   int n, k;
   cin >> n >> k;
@@ -15,6 +19,35 @@ void solve() {
     int tmp;
     cin >> tmp;
     cout << (tmp % g == 0 ? "YES" : "NO") << endl;
+  }
+}
+*/
+
+
+// Now use dp
+int dp[360];
+int a[10];
+int n, k;
+
+void dpFn(int v) {
+  if (dp[v]) return;
+  dp[v] = 1;
+  for (int i = 0; i < n; ++i) {
+    dpFn((v + a[i]) % 360);
+  }
+}
+
+void solve() {
+  cin >> n >> k;
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  dpFn(0);
+
+  for (int i = 0; i < k; ++i) {
+    int tmp;
+    cin >> tmp;
+    cout << (dp[tmp] ? "YES" : "NO") << endl;
   }
 }
 
